276e287f0115642e8506cde82f39c38f6f5901f4
Gaultier Laperche <gaultier.laperche@zuuno.fr>
2015-07-21 15:15:27 +0200

    .

--- lib/octobat/customer.rb
+++ lib/octobat/customer.rb
@@ -3,5 +3,10 @@ module Octobat
     include Octobat::APIOperations::List
     include Octobat::APIOperations::Create
     include Octobat::APIOperations::Update
+    
+    def invoices
+      Invoice.all({ :customer => id }, @api_key)
+    end
+    
   end
 end
