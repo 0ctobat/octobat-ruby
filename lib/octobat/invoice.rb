18852bee0d8d1dbe7447c094ef19d2dd7d06c8c5
Gaultier Laperche <gaultier.laperche@zuuno.fr>
2015-10-13 18:01:26 +0200

    Update/send invoice by email

--- lib/octobat/invoice.rb
+++ lib/octobat/invoice.rb
@@ -2,9 +2,15 @@ module Octobat
   class Invoice < APIResource
     include Octobat::APIOperations::List
     include Octobat::APIOperations::Create
+    include Octobat::APIOperations::Update
 
     def pay(payment_data)
       response, api_key = Octobat.request(:patch, pay_url, @api_key, {payment: payment_data})
+      refresh_from(response, api_key)
+    end
+    
+    def send(enforce_errors = false)
+      response, api_key = Octobat.request(:post, send_url, @api_key, {enforce_errors: enforce_errors})
       refresh_from(response, api_key)
     end
 
@@ -12,6 +18,10 @@ module Octobat
 
     def pay_url
       url + '/pay'
+    end
+    
+    def send_url
+      url + '/send'
     end
   end
 end
