5ebdf38c95108443fa0b04ab9eb6f1cffded950c
Gaultier Laperche <gaultier.laperche@zuuno.fr>
2016-07-12 11:06:41 +0200

    save a transaction

--- lib/octobat/transaction.rb
+++ lib/octobat/transaction.rb
@@ -2,6 +2,7 @@ module Octobat
   class Transaction < APIResource
     extend Octobat::APIOperations::List
     include Octobat::APIOperations::Create
+    include Octobat::APIOperations::Update
 
     def items(params = {})
       Item.list(params.merge({ :transaction => id }), @api_key)
