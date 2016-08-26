a396aea8690010d55ae3e39ebc2a01a7f02a1d90
Nicolas Guillemain <nicolas.guillemain@zuuno.fr>
2016-08-26 16:45:35 +0200

    Numbering sequences

--- lib/octobat/invoice_numbering_sequence.rb
+++ lib/octobat/invoice_numbering_sequence.rb
@@ -1,5 +1,18 @@
 module Octobat
   class InvoiceNumberingSequence < APIResource
     extend Octobat::APIOperations::List
+    include Octobat::APIOperations::Create
+    include Octobat::APIOperations::Update
+
+    def set_to_default
+      response, api_key = Octobat.request(:patch, set_to_default_url, @api_key)
+      refresh_from(response, api_key)
+    end
+
+    private
+
+      def set_to_default_url
+        url + '/default'
+      end
   end
 end
