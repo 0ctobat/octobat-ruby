8c3751a201fb55e43bc0ea65ab6fb3bd63a2d564
Gaultier Laperche <pinturicchio@genepi.home>
2017-03-23 20:22:56 +0100

    Document template preview

--- lib/octobat/document_template.rb
+++ lib/octobat/document_template.rb
@@ -18,6 +18,18 @@ module Octobat
       response, api_key = Octobat.request(:delete, url, @api_key)
       refresh_from(response, api_key)
     end
+    
+    def self.preview(params = {}, opts={})
+      api_key, headers = Util.parse_opts(opts)
+      api_key ||= @api_key
+      opts[:api_key] = api_key
+
+      instance = self.new(nil, opts)
+
+      response, api_key = Octobat.request(:get, url + '/preview', api_key, params)
+      instance.refresh_from(response, api_key)
+      instance
+    end
 
     private
 
