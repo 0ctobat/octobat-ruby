4463dc50bfb8ff9f2e323f629b1f1d26d258929f
matthieuberger <berger.matthieu@etu.unice.fr>
2017-04-11 18:20:20 +0200

    Added customer delete

--- lib/octobat/customer.rb
+++ lib/octobat/customer.rb
@@ -3,6 +3,7 @@ module Octobat
     extend Octobat::APIOperations::List
     include Octobat::APIOperations::Create
     include Octobat::APIOperations::Update
+    include Octobat::APIOperations::Delete
 
     def invoices(params = {})
       Invoice.all(params.merge({ :customer => id }), @api_key)
@@ -14,6 +15,11 @@ module Octobat
 
     def payment_sources(params = {})
       PaymentSource.all(params.merge({ :customer => id }), @api_key)
+    end
+
+    def delete
+      response, api_key = Octobat.request(:delete, url, @api_key)
+      refresh_from(response, api_key)
     end
 
   end
