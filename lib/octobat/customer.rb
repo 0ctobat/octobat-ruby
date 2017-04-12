2fe7318074fe36fb4ab780f74126af5e9142b47c
Gaultier Laperche <pinturicchio@genepi.home>
2017-04-12 14:18:08 +0200

    remove duplicate

--- lib/octobat/customer.rb
+++ lib/octobat/customer.rb
@@ -16,11 +16,5 @@ module Octobat
     def payment_sources(params = {})
       PaymentSource.all(params.merge({ :customer => id }), @api_key)
     end
-
-    def delete
-      response, api_key = Octobat.request(:delete, url, @api_key)
-      refresh_from(response, api_key)
-    end
-
   end
 end
