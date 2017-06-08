00af7ec0ac1b13b1e14622dc7f2ec184e243ef84
Gaultier Laperche <pinturicchio@genepi.home>
2017-06-08 17:22:46 +0200

    csv export transactions

--- lib/octobat/transaction.rb
+++ lib/octobat/transaction.rb
@@ -7,5 +7,17 @@ module Octobat
     def items(params = {})
       Item.list(params.merge({ :transaction => id }), @api_key)
     end
+    
+    def self.csv_export(params = {}, opts={})
+      api_key, headers = Util.parse_opts(opts)
+      api_key ||= @api_key
+      opts[:api_key] = api_key
+
+      instance = self.new(nil, opts)
+
+      response, api_key = Octobat.request(:post, url + '/csv_export', api_key, params)
+      return true
+    end
+    
   end
 end
