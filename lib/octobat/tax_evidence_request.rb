e0f680d047c0b7af7d8e5d9fb96a24d06fd6854b
Gaultier Laperche <gaultier@octobat.com>
2018-03-08 11:41:31 +0100

    Add tax evidence requests for suppliers, bump to 2.0.10

--- lib/octobat/tax_evidence_request.rb
+++ lib/octobat/tax_evidence_request.rb
@@ -1,5 +1,17 @@
 module Octobat
   class TaxEvidenceRequest < APIResource
     include Octobat::APIOperations::Create
+    
+    def self.for_supplier(params = {}, opts={})
+      api_key, headers = Util.parse_opts(opts)
+      api_key ||= @api_key
+      opts[:api_key] = api_key
+
+      instance = self.new(nil, opts)
+
+      response, api_key = Octobat.request(:post, url + '/for_supplier', api_key, params)
+      instance.refresh_from(response, api_key)
+      instance
+    end
   end
 end
