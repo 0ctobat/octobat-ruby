9190f913c9ac6d270475b534a13f90360b4b0a6d
Gaultier Laperche <gaultier@octobat.com>
2018-04-10 16:36:04 +0200

    Payouts CSV exports

--- lib/octobat/payout.rb
+++ lib/octobat/payout.rb
@@ -5,5 +5,16 @@ module Octobat
     def balance_transactions(params = {})
       BalanceTransaction.list(params.merge({ payout: id }), @api_key)
     end
+    
+    def self.csv_export(params = {}, opts={})
+      api_key, headers = Util.parse_opts(opts)
+      api_key ||= @api_key
+      opts[:api_key] = api_key
+
+      instance = self.new(nil, opts)
+
+      response, api_key = Octobat.request(:post, url + '/csv_export', api_key, params)
+      return true
+    end
   end
 end
