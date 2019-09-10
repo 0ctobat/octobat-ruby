e9d8b6348af91c385bcf21a365ee2d439a3702c1
Gaultier Laperche <gaultier@octobat.com>
2019-09-10 14:56:58 +0200

    add setup intent support for orders

--- lib/octobat/order.rb
+++ lib/octobat/order.rb
@@ -11,6 +11,12 @@ module Octobat
       response, api_key = Octobat.request(:patch, update_payment_intent_status_url, @api_key, payment_intent_status_data)
       refresh_from(response, api_key)
     end
+    
+    def update_setup_intent_status(setup_intent_status_data = {})
+      response, api_key = Octobat.request(:patch, update_setup_intent_status_url, @api_key, setup_intent_status_data)
+      refresh_from(response, api_key)
+    end
+    
 
     private
       def expire_url
@@ -19,6 +25,10 @@ module Octobat
 
       def update_payment_intent_status_url
         url + '/payment_intent_status'
+      end
+      
+      def update_setup_intent_status_url
+        url + '/setup_intent_status'
       end
 
   end
