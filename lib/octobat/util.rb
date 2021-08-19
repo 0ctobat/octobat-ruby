47846d2faf8692bbdee5c4f3a48cd4a83445748f
Gaultier Laperche <>
2021-08-19 12:33:05 +0200

    URI escape is deprecated

--- lib/octobat/util.rb
+++ lib/octobat/util.rb
@@ -127,7 +127,7 @@ module Octobat
     end
 
     def self.url_encode(key)
-      URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      URI.encode_www_form_component(key.to_s)
     end
 
     def self.flatten_params(params, parent_key=nil)
